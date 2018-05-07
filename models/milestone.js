8acab91311729e92a6293f672cb398f97b65f66e
Alex McBride <xandermcbride@gmail.com>
2018-05-07 10:55:32 +0100

    Tightened up the milestones save code

--- models/milestone.js
+++ models/milestone.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 var moment = require('moment');
 
+var Resource = require('./resource');
+
 var milestoneSchema = mongoose.Schema({
     projectId: { type: String, required: true },
+    userId: {type: String, required: true},
     name: { type: String, required: [true, 'Name cannot be empty'] },
     due: { type: Date, required: [true, 'Due cannot be empty'] },
     completed: { 
@@ -11,24 +14,43 @@ var milestoneSchema = mongoose.Schema({
             validator: function(v) {
                 return v && moment(v).isBefore();
             },
-            message: 'Milestone cannot be completed in future'
+            message: 'Milestone cannot be completed in the future'
         } 
     },
     created: { type: Date, required: true, default: Date.now }
 });
 
-milestoneSchema.methods.duePretty = function() {
+milestoneSchema.methods.create = function(done) {
+    var params = {
+        userId: this.userId,
+        resourceId: this._id,
+        resourceType: 'milestone',
+        accessType: 'owner'
+    };
+
+    this.save(function(err) {
+        if (err) {
+            return done(err);
+        }
+
+        var resource = new Resource(params);
+        resource.save(function(err) {
+            done(err);
+        });
+    });
+}
+
+milestoneSchema.virtual('duePretty').get(function() {
     return moment(this.due).fromNow();
-};
+});
 
-milestoneSchema.methods.isLate = function () {
-    var now = new Date();
-    return this.completed == null && moment(this.due).isBefore(now);
-};
+milestoneSchema.virtual('isLate').get(function() {
+    return this.completed == null && moment(this.due).isBefore(new Date());
+});
 
-milestoneSchema.methods.isComplete = function () {
+milestoneSchema.virtual('isComplete').get(function () {
     return this.completed != null
-};
+});
 
 var Milestone = mongoose.model('Milestone', milestoneSchema);
 
--- routes/milestones.js
+++ routes/milestones.js
@@ -3,21 +3,22 @@ var router = express.Router();
 var Milestone = require('../models/milestone');
 var authorize = require('../utils/authorize');
 
-router.get('/create/:projectId', authorize, function (req, res) {
+router.get('/create/:id', authorize('project'), function (req, res) {
     var milestone = new Milestone({
-        projectId: req.params.projectId, 
+        projectId: req.params.id, 
         due: new Date()});
     res.render('milestones/create', { milestone: milestone });
 });
 
-router.post('/create/:projectId', authorize, function (req, res) {
-    var projectId = req.params.projectId;
+router.post('/create/:id', authorize('project'), function (req, res) {
+    var projectId = req.params.id;
     var milestone = new Milestone({
+        userId: req.userManager.userId(),
         projectId: projectId, 
         name: req.body.name,
         due: req.body.due
     });
-    milestone.save(function(err) {
+    milestone.create(function(err) {
         if (err) {
             res.render('milestones/create', { milestone: milestone, errors: err.errors });
         }
